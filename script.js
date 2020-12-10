cf0191ed1b9522e6a4523df384cd3f332b42bdf0
Kray93 <kevinwillig@gmail.com>
2020-12-09 22:02:02 -0800

    added arrays and if/else

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 // Assignment Code
-var lower = ['abcdefghijklmnopqrstuvwxyz']
-var upper = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
-var number = ['0123456789']
-var special = ['!@#$%^&*()_+=-<>/?']
 var generateBtn = document.querySelector("#generate");
-
-// ask password length
-var passwordLength = prompt ("Please choose password length between 6-20 characters!")
-// ask if user want to use lower case letters
-var askLower = prompt("Do you want lower case letters?")
-// ask if user wants upper case letters
-var askUpper = prompt("Do you want upper case letters?")
-// ask if user wants numbers
-var askNumber = prompt("Do you want numbers?")
-// ask if user wants special characters
-var askSpecial = prompt("Do you want special characters?")
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -29,14 +12,80 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-    
-    
-    
-    
-    
-    return compilation
+
+// generate password function
+function generatePassword() {   
+// list of variables and arrays
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "<", ">", "_", "-"];
+var okayChar = [];
+var randomPass = [];
+var generateBtn = document.querySelector("#generate");
+
+// ask password length
+// TODO: if less than 8 or more than 128, prompt for a new selection
+var userLength = prompt ("Please choose password length between 8-128 characters!")
+console.log("User length selection: " + userLength);
+
+
+// ask if user want to use lower case letters
+var askLower = confirm("Do you want lower case letters?")
+
+// add or exclude lower case letters in password
+if (askLower===true){
+    okayChar.push(lowerCase)
+    console.log("Lower case OK: " + true);
+} else {
+    console.log("Lower case OK: " + false);
 }
 
 
-"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+// ask if user wants upper case letters
+var askUpper = confirm("Do you want upper case letters?")
+
+// add or exclude upper case letters in password
+if (askUpper===true){
+    okayChar.push(upperCase)
+    console.log("Upper case OK: " + true);
+} else {
+    console.log("Upper case OK: " + false);
+}
+
+
+// ask if user wants numbers
+var askNumber = confirm("Do you want numbers?")
+
+// add or exclude numbers in password
+if (askNumber===true){
+    okayChar.push(numbers)
+    console.log("Numbers OK: " + true);
+} else {
+    console.log("Numbers OK: " + false);
+}
+
+
+// ask if user wants special characters
+var askSpecial = confirm("Do you want special characters?")
+
+// add or exclude special characters in password
+if (askSpecial===true){
+    okayChar.push(specialChar)
+    console.log("Special characters OK: " + true);
+} else {
+    console.log("Special characters OK: " + false);
+}
+
+
+// loop to randomly select okayChar for randomPass
+ for (var i = 0; i < userLength; i++) {
+    randomPass[i] = okayChar[Math.floor(Math.random() * okayChar.length)];   
+    // FIXME:not pulling userLength in randomPass.. 
+ }   
+//   TODO: array concatenate. create a string from array randomPass
+    
+    
+    return randomPass
+    // .toString()???
+}
