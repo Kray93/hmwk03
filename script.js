f362b71d6f281a536e9a077f4a859c023785479c
Kray93 <kevinwillig@gmail.com>
2020-12-09 19:55:42 -0800

    added prompts

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ var number = ['0123456789']
 var special = ['!@#$%^&*()_+=-<>/?']
 var generateBtn = document.querySelector("#generate");
 
+// ask password length
+var passwordLength = prompt ("Please choose password length between 6-20 characters!")
+// ask if user want to use lower case letters
+var askLower = prompt("Do you want lower case letters?")
+// ask if user wants upper case letters
+var askUpper = prompt("Do you want upper case letters?")
+// ask if user wants numbers
+var askNumber = prompt("Do you want numbers?")
+// ask if user wants special characters
+var askSpecial = prompt("Do you want special characters?")
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,8 +30,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-    // ask password length
-    var passwordLength = prompt ("Please choose password length between 6-20 characters!")
+    
     
     
     
