7f4709fa228b8bdbb7158fcf7d998acacbe743e9
Kray93 <kevinwillig@gmail.com>
2020-12-09 19:49:49 -0800

    added var characters

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Assignment Code
+var lower = ['abcdefghijklmnopqrstuvwxyz']
+var upper = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+var number = ['0123456789']
+var special = ['!@#$%^&*()_+=-<>/?']
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +16,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    // ask password length
+    var passwordLength = prompt ("Please choose password length between 6-20 characters!")
+    
+    
+    
+    
+    return compilation
+}
+
+
+"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
